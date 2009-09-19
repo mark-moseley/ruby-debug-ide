1278feb18473136f22cd7d63e30b7f10734d4a13
Mark Moseley <mark@fast-software.com>
2009-09-18 18:45:06 -0700

    RubyForge sync

---
+++ ext/mkrf_conf.rb
@@ -0,0 +1,28 @@
+jruby = defined?(JRUBY_VERSION) || (defined?(RUBY_ENGINE) && 'jruby' == RUBY_ENGINE)
+unless jruby
+  require 'rubygems'
+  require 'rubygems/command.rb'
+  require 'rubygems/dependency_installer.rb'
+
+  begin
+    Gem::Command.build_args = ARGV
+    rescue NoMethodError
+  end
+
+  inst = Gem::DependencyInstaller.new
+  begin
+    if RUBY_VERSION < "1.9"
+      inst.install "ruby-debug-base", "~> 0.10.3"
+    else
+      inst.install "ruby-debug-base19", "~> 0.11.15" 
+    end
+    rescue
+      exit(1)
+  end
+end
+
+# create dummy rakefile to indicate success
+f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")
+f.write("task :default\n")
+f.close
+
